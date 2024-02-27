4041f7adabaff54a42d8c388de9a6660aa378c70
ridvanmahmudbiber <ridvanmahmudbiber@gmail.com>
2024-02-27 20:56:27 +0300

    first commit

---
+++ app/src/main/java/com/example/artbookkotlin/ArtActivity.kt
@@ -0,0 +1,25 @@
+package com.example.artbookkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.artbookkotlin.databinding.ActivityArtBinding
+
+class ArtActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityArtBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityArtBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun saveButtonClicked(view: View) {
+
+    }
+    fun selectImage(view: View) {
+
+    }
+}
